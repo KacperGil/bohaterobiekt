6242260d1e441266b6c0940cfe8247c1d1b60bba
KacperGil <kacpergil1610@gmail.com>
2022-11-24 19:59:25 +0100

    obiekt bohater

---
+++ src/Bohater.java
@@ -0,0 +1,16 @@
+public class Bohater {
+  String nick;
+  String abilities;
+  int lvl;
+  double damage;
+  boolean fame;
+
+    Bohater(String nick, String abilities, int lvl, double damage, boolean fame){
+        this.nick=nick;
+        this.abilities=abilities;
+        this.lvl=lvl;
+        this.damage=damage;
+        this.fame=fame;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Bohater bohater1=new Bohater("NeO", "mag", 1, 22.5, true);
+        System.out.println(bohater1);
+    }
+}
